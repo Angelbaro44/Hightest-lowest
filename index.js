3262c8d4a6628b255f3ffe8031b410409770c1e3
Angel Baro <angelbaro44@gmail.com>
2021-04-24 16:42:52 -0400

    Kata Finished

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-function highAndLow(numbers){
+
+
+function highAndLow(numbers) {
       let newlist = [0]
-    newlist=numbers.split(' ').sort((a,b)=> b-a)
-    return( newlist[0]+' '+newlist[newlist.length-1])
-    
-    }
+      newlist = numbers.split(' ').sort((a, b) => b - a)
+      return (newlist[0] + ' ' + newlist[newlist.length - 1])
+
+}
 
 
 
